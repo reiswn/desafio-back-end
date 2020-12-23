b4b3253bef1a2207c7c6fd2791ca59c02cc1cf54
Willian <reiswn@gmail.com>
2020-12-23 11:57:05 -0300

    transaction_type seed

--- desafio-back-end/db/seeds.rb
+++ desafio-back-end/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+TransactionType.create!(id: 1, description: "Débito", kind: "Entrada", sign: "+")
+TransactionType.create!(id: 2, description: "Boleto", kind: "Saída", sign: "-")
+TransactionType.create!(id: 3, description: "Financiamento", kind: "Saída", sign: "-")
+TransactionType.create!(id: 4, description: "Crédito", kind: "Entrada", sign: "+")
+TransactionType.create!(id: 5, description: "Recebimento Empréstimo", kind: "Entrada", sign: "+")
+TransactionType.create!(id: 6, description: "Vendas", kind: "Entrada", sign: "+")
+TransactionType.create!(id: 7, description: "Recebimento TED", kind: "Entrada", sign: "+")
+TransactionType.create!(id: 8, description: "Recebimento DOC", kind: "Entrada", sign: "+")
+TransactionType.create!(id: 9, description: "Aluguel", kind: "Saída", sign: "-")
