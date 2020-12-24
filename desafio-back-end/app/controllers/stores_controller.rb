0ec6335c96575a6a772cb591482e6bf528505938
Willian <reiswn@gmail.com>
2020-12-24 00:41:28 -0300

    stores index and show created

--- desafio-back-end/app/controllers/stores_controller.rb
+++ desafio-back-end/app/controllers/stores_controller.rb
@@ -1,2 +1,24 @@
 class StoresController < ApplicationController
+  def index
+    @stores = Transaction.select("store_name").group("store_name")
+  end
+
+  def show
+    @transactions = Transaction.where(store_name: params[:store_name]).order(:transaction_datetime)
+    if @transactions.size != 0
+      @saldo = 0
+
+      @transactions.each do |transaction|
+        if (transaction.transaction_type.sign == "+")
+          @saldo += transaction.value
+        else
+          @saldo -= transaction.value
+        end
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to stores_url, flash: { error: "Store " + params[:store_name] + " not found." } }
+      end
+    end
+  end
 end
--- desafio-back-end/config/routes.rb
+++ desafio-back-end/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :transactions
   resources :transaction_types
   post "transactions/upload", to: "transactions#upload"
+  get "stores" => "stores#index"
+  get "stores/:store_name" => "stores#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
